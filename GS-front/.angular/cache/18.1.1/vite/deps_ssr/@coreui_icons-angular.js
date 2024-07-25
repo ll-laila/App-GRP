d19695307ddf8bb3fd5a0f01ec6ceff703ddcb04
OU_chaima <ouazzanchaimae@gmail.com>
2024-07-25 15:48:36 +0000

    pdf

---
+++ GS-front/.angular/cache/18.1.1/vite/deps_ssr/@coreui_icons-angular.js
@@ -0,0 +1,24 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  IconComponent,
+  IconDirective,
+  IconModule,
+  IconSetModule,
+  IconSetService
+} from "./chunk-5W4P4V6L.js";
+import "./chunk-BYKQZPBR.js";
+import "./chunk-BFLGF2G2.js";
+import "./chunk-TZZCXSTZ.js";
+import "./chunk-5GECNL2D.js";
+import "./chunk-44LTBBPH.js";
+import "./chunk-IXHKNY7D.js";
+import "./chunk-M67RBMTJ.js";
+import "./chunk-KA5JLFAN.js";
+export {
+  IconComponent,
+  IconDirective,
+  IconModule,
+  IconSetModule,
+  IconSetService
+};
+//# sourceMappingURL=@coreui_icons-angular.js.map
