c60dcfbef2268b874dfc2fb3632d616c9003be94
ll-laila <lailatimasli1@gmail.com>
2024-07-29 09:27:56 +0100

    notifs

---
+++ GS-front/.angular/cache/18.1.1/vite/deps_ssr/@angular_platform-browser_animations.js
@@ -0,0 +1,28 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BrowserAnimationsModule,
+  InjectableAnimationEngine,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations
+} from "./chunk-WC6WI5RW.js";
+import "./chunk-WDLJQSEQ.js";
+import "./chunk-HA5HH2FO.js";
+import "./chunk-ZA26Q2WV.js";
+import "./chunk-6NMH7JIH.js";
+import {
+  ANIMATION_MODULE_TYPE
+} from "./chunk-QUOXCSOH.js";
+import "./chunk-44LTBBPH.js";
+import "./chunk-IXHKNY7D.js";
+import "./chunk-M67RBMTJ.js";
+import "./chunk-KA5JLFAN.js";
+export {
+  ANIMATION_MODULE_TYPE,
+  BrowserAnimationsModule,
+  NoopAnimationsModule,
+  provideAnimations,
+  provideNoopAnimations,
+  InjectableAnimationEngine as ɵInjectableAnimationEngine
+};
+//# sourceMappingURL=@angular_platform-browser_animations.js.map
